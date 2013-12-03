4c5476fe26833f0da3cdddb95e70ffc963b8f230
David Gowrie <david.gowrie@gmail.com>
2013-12-02 23:45:16 -0500

    ARRAYS: Mutator Methods completed, Page 35 - spice - unshift

--- jsjs.js
+++ jsjs.js
@@ -1,6 +1,6 @@
 // JS JS project
 
-// ! Arrays
+// ! ARRAYS
 
 var tasks;
 
@@ -39,3 +39,68 @@ tasks
 
 tasks.sort();
 //["Pay the gas bill", "Remember to walk the dog", "Write best-selling novel"]
+
+// ! Splice
+var tasks = [
+	"Pay phone bill",
+	"Write best-selling novel",
+	"Walk the dog"
+];
+
+tasks.splice(1, 1, "World domination");
+//["Write best-selling novel"]
+
+tasks;
+//["Pay phone bill", "World domination", "Walk the dog"]
+
+// Splice on multiple items
+var tasks = [
+	"Pay phone bill",
+	"Write best-selling novel",
+	"Walk the dog"
+];
+
+tasks.splice(1, 1, "World domination", "Rotate tires", "Hire hit squad");
+//["Write best-selling novel"]
+
+tasks;
+//["Pay phone bill", "World domination", "Rotate tires", "Hire hit squad", "Walk the dog"]
+
+// Splice out without adding
+var tasks, task;
+
+tasks = [
+	"Pay phone bill",
+	"Write best-selling novel",
+	"Walk the dog"
+];
+task = tasks.splice(1, 1);
+
+alert("REMINDER! Don't forget to: " + task);
+
+tasks;
+//["Pay phone bill", "Walk the dog"]
+task;
+//["Write best-selling novel"]
+
+
+// ! Unshift
+var tasks, len;
+
+tasks = [
+	"Pay phone bill",
+	"Write best-selling novel",
+	"Walk the dog"
+];
+len = tasks.unshift("Defeat nemesis", "Pick up dry cleaning");
+alert("You now have " + len + " tasks to complete: " + tasks.join(", "));
+
+tasks;
+//["Defeat nemesis", "Pick up dry cleaning", "Pay phone bill", "Write best-selling novel", "Walk the dog"]
+
+len;
+//5
+
+
+
+
